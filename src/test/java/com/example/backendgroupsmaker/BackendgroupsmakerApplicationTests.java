39ac4171583619f2eb0402cec78d4c31872b2748
Quentin384 <zampieri.quentin38@gmail.com>
2025-06-12 10:42:27 +0200

    starting project

---
+++ src/test/java/com/example/backendgroupsmaker/BackendgroupsmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendgroupsmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendgroupsmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
